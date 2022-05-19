3346e75c10eded1bdffdb28c4e7e1710faeca1a1
Kheona Taracatac <100635968+st21157@users.noreply.github.com>
2022-05-20 10:03:10 +1200

    added entries

--- layout.py
+++ layout.py
@@ -9,12 +9,17 @@ from tkinter import *
 
 root = Tk()
 
-# labels
+# labels + entries
 Label(root, text="Leader").grid(row=1, column=1)
-Label(root, text="# of Campers").grid(row=3, column=1)
-Label(root, text="Location").grid(row=1, column=3)
-Label(root, text="Weather").grid(row=3, column=3)
+ent_leader = Entry(root).grid(row=1, column=2)
 
+Label(root, text="# of Campers").grid(row=3, column=1)
+
+Label(root, text="Location").grid(row=1, column=3)
+ent_location = Entry(root).grid(row=1, column=4)
+
+Label(root, text="Weather").grid(row=3, column=3)
+ent_weather = Entry(root).grid(row=3, column=4)
 
 
 
