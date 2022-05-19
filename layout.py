29d0e22b4d3740c0961cf7ce6cc30653804b300e
Kheona Taracatac <100635968+st21157@users.noreply.github.com>
2022-05-20 10:16:00 +1200

    added more labels (for details grid)

--- layout.py
+++ layout.py
@@ -21,8 +21,15 @@ ent_location = Entry(root).grid(row=1, column=4)
 Label(root, text="Weather").grid(row=3, column=3)
 ent_weather = Entry(root).grid(row=3, column=4)
 
+# for grid
+Label(root, text="Group").grid(row=6,column=1)
+Label(root, text="Leader").grid(row=6, column=2)
+Label(root, text="Campers").grid(row=6, column=3)
+Label(root, text="Location").grid(row=6, column=4)
+Label(root, text="Weather").grid(row=6, column=5)
 
-# buttons
+
+# buttons >> need commands
 Button(root, text="Add").grid(row=4, column=1)
 Button(root, text="Delete").grid(row=4, column=2)  # add combobox in same cell
 Button(root, text="View Details").grid(row=4, column=3)
