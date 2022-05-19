28e676ba068e866a29457564b35b649e81957d2e
Kheona Taracatac <100635968+st21157@users.noreply.github.com>
2022-05-20 09:57:17 +1200

    added label

--- layout.py
+++ layout.py
@@ -9,7 +9,13 @@ from tkinter import *
 
 root = Tk()
 
+# labels
+Label(root, text="Leader").grid(row=1, column=1)
+Label(root, text="# of Campers").grid(row=3, column=1)
+Label(root, text="Location").grid(row=1, column=3)
+Label(root, text="Weather").grid(row=3, column=3)
 
 
 
-root.mainloop()
+
+root.mainloop() # exclude when importing
