a7976d636bd09ca9d87911c01c37b40e5567e01f
Kheona Taracatac <100635968+st21157@users.noreply.github.com>
2022-05-20 10:09:48 +1200

    added more buttons

--- layout.py
+++ layout.py
@@ -11,7 +11,7 @@ root = Tk()
 
 # labels + entries
 Label(root, text="Leader").grid(row=1, column=1)
-ent_leader = Entry(root).grid(row=1, column=2)
+ent_leader = Entry(root).grid(row=1, column=2)  # user input stored as variables
 
 Label(root, text="# of Campers").grid(row=3, column=1)
 
@@ -21,6 +21,12 @@ ent_location = Entry(root).grid(row=1, column=4)
 Label(root, text="Weather").grid(row=3, column=3)
 ent_weather = Entry(root).grid(row=3, column=4)
 
+
+# buttons
+Button(root, text="Add").grid(row=4, column=1)
+Button(root, text="Delete").grid(row=4, column=2)  # add combobox in same cell
+Button(root, text="View Details").grid(row=4, column=3)
+Button(root, text="Quit").grid(row=4, column=4)
 
 
 root.mainloop() # exclude when importing
